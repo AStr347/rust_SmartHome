6782fa3187f63b61238ce694674026c7788609a3
AStr347 <anton3000str@mail.ru>
2022-09-12 11:02:35 +0300

    feat(ia_home): homeerrors through thiserror crate

--- lib/ia_home/src/homeerrors.rs
+++ lib/ia_home/src/homeerrors.rs
@@ -1,19 +1,9 @@
-use std::{error::Error, fmt::Display};
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum HomeErrors {
+    #[error("element with name '{0}' not exist")]
     NotExist(String),
+    #[error("element with name '{0}' already exist")]
     AlreadyExist(String),
 }
-
-impl Display for HomeErrors {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let msg = match self {
-            HomeErrors::NotExist(x) => format!("element with name '{}' not exist", x),
-            HomeErrors::AlreadyExist(x) => format!("element with name '{}' already exist", x),
-        };
-        f.write_str(&msg)
-    }
-}
-
-impl Error for HomeErrors {}
